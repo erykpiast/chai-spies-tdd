851fe08b67aa4172bf1d349630ee207e57257a67
Eryk Napierała <eryk.piast@gmail.com>
2015-05-06 21:37:30 +0200

    remove chai-spies dependency

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-import spies from 'chai-spies';
-
 export default function(chai, utils) {
-    chai.use(spies);
-    
     utils.addMethod(chai.assert, 'calledOnce', function(actual, expected, msg) {
         new chai.Assertion(actual).to.have.been.called.once(msg);
     });
