6ab36ed8361b1798b19aa2c1968c76095ca935ac
Eryk Napierała <eryk.piast@gmail.com>
2015-05-11 16:09:18 +0200

    add a few methods

--- src/chai-spies-tdd.js
+++ src/chai-spies-tdd.js
@@ -6,4 +6,20 @@ export default function (chai, utils) {
     utils.addMethod(chai.assert, 'notCalledOnce', function (actual, expected, msg) {
         new chai.Assertion(actual).not.to.have.been.called.once(msg);
     });
+
+    utils.addMethod(chai.assert, 'calledTwice', function (actual, expected, msg) {
+        new chai.Assertion(actual).to.have.been.called.twice(msg);
+    });
+
+    utils.addMethod(chai.assert, 'notCalledTwice', function (actual, expected, msg) {
+        new chai.Assertion(actual).not.to.have.been.called.twice(msg);
+    });
+
+    utils.addMethod(chai.assert, 'calledExactly', function (actual, expected, msg) {
+        new chai.Assertion(actual).to.have.been.called.exactly(expected, msg);
+    });
+
+    utils.addMethod(chai.assert, 'notCalledExactly', function (actual, expected, msg) {
+        new chai.Assertion(actual).not.to.have.been.called.exactly(expected, msg);
+    });
 }
