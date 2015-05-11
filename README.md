c5846c2ef426c8a7f7218efb22c538878456e9c1
Eryk Napierała <eryk.piast@gmail.com>
2015-05-11 16:29:50 +0200

    Build dist/

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ TDD-style interface for [chai-spies](https://github.com/chaijs/chai-spies).
 
  * `assert.calledOnce`
  * `assert.notCalledOnce`
+ * `assert.calledTwice`
+ * `assert.notCalledTwice`
+ * `assert.calledExactly`
+ * `assert.notCalledExactly`
+ * `assert.calledWith`
+ * `assert.notCalledWith`
--- dist/chai-spies-tdd.js
+++ dist/chai-spies-tdd.js
@@ -12,6 +12,30 @@ exports['default'] = function (chai, utils) {
     utils.addMethod(chai.assert, 'notCalledOnce', function (actual, expected, msg) {
         new chai.Assertion(actual).not.to.have.been.called.once(msg);
     });
+
+    utils.addMethod(chai.assert, 'calledTwice', function (actual, expected, msg) {
+        new chai.Assertion(actual).to.have.been.called.twice(msg);
+    });
+
+    utils.addMethod(chai.assert, 'notCalledTwice', function (actual, expected, msg) {
+        new chai.Assertion(actual).not.to.have.been.called.twice(msg);
+    });
+
+    utils.addMethod(chai.assert, 'calledExactly', function (actual, expected, msg) {
+        new chai.Assertion(actual).to.have.been.called.exactly(expected, msg);
+    });
+
+    utils.addMethod(chai.assert, 'notCalledExactly', function (actual, expected, msg) {
+        new chai.Assertion(actual).not.to.have.been.called.exactly(expected, msg);
+    });
+
+    utils.addMethod(chai.assert, 'calledWith', function (actual, expected, msg) {
+        new chai.Assertion(actual).to.have.been.called['with'](expected, msg);
+    });
+
+    utils.addMethod(chai.assert, 'notCalledWith', function (actual, expected, msg) {
+        new chai.Assertion(actual).not.to.have.been.called['with'](expected, msg);
+    });
 };
 
 module.exports = exports['default'];
