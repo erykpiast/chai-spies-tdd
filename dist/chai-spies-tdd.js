5b20e6f1f726603c526f4613be3ab978c26482fe
Eryk Napierała <eryk.piast@gmail.com>
2016-10-14 13:34:36 +0200

    update compiled files

--- dist/chai-spies-tdd.js
+++ dist/chai-spies-tdd.js
@@ -5,35 +5,47 @@ Object.defineProperty(exports, "__esModule", {
 });
 
 exports.default = function (chai, utils) {
-  utils.addMethod(chai.assert, 'calledOnce', function (actual, expected, msg) {
-    new chai.Assertion(actual).to.have.been.called.once(msg);
+  utils.addMethod(chai.assert, 'calledOnce', function (actual, expected) {
+    new chai.Assertion(actual).to.have.been.called.once();
   });
 
-  utils.addMethod(chai.assert, 'notCalledOnce', function (actual, expected, msg) {
-    new chai.Assertion(actual).not.to.have.been.called.once(msg);
+  utils.addMethod(chai.assert, 'notCalledOnce', function (actual, expected) {
+    new chai.Assertion(actual).not.to.have.been.called.once();
   });
 
-  utils.addMethod(chai.assert, 'calledTwice', function (actual, expected, msg) {
-    new chai.Assertion(actual).to.have.been.called.twice(msg);
+  utils.addMethod(chai.assert, 'calledTwice', function (actual, expected) {
+    new chai.Assertion(actual).to.have.been.called.twice();
   });
 
-  utils.addMethod(chai.assert, 'notCalledTwice', function (actual, expected, msg) {
-    new chai.Assertion(actual).not.to.have.been.called.twice(msg);
+  utils.addMethod(chai.assert, 'notCalledTwice', function (actual, expected) {
+    new chai.Assertion(actual).not.to.have.been.called.twice();
   });
 
-  utils.addMethod(chai.assert, 'calledExactly', function (actual, expected, msg) {
-    new chai.Assertion(actual).to.have.been.called.exactly(expected, msg);
+  utils.addMethod(chai.assert, 'calledExactly', function (actual, expected) {
+    new chai.Assertion(actual).to.have.been.called.exactly(expected);
   });
 
-  utils.addMethod(chai.assert, 'notCalledExactly', function (actual, expected, msg) {
-    new chai.Assertion(actual).not.to.have.been.called.exactly(expected, msg);
+  utils.addMethod(chai.assert, 'notCalledExactly', function (actual, expected) {
+    new chai.Assertion(actual).not.to.have.been.called.exactly(expected);
   });
 
-  utils.addMethod(chai.assert, 'calledWith', function (actual, expected, msg) {
-    new chai.Assertion(actual).to.have.been.called.with(expected, msg);
+  utils.addMethod(chai.assert, 'calledWith', function (actual) {
+    var _to$have$been$called;
+
+    for (var _len = arguments.length, expected = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+      expected[_key - 1] = arguments[_key];
+    }
+
+    (_to$have$been$called = new chai.Assertion(actual).to.have.been.called).with.apply(_to$have$been$called, expected);
   });
 
-  utils.addMethod(chai.assert, 'notCalledWith', function (actual, expected, msg) {
-    new chai.Assertion(actual).not.to.have.been.called.with(expected, msg);
+  utils.addMethod(chai.assert, 'notCalledWith', function (actual) {
+    var _not$to$have$been$cal;
+
+    for (var _len2 = arguments.length, expected = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
+      expected[_key2 - 1] = arguments[_key2];
+    }
+
+    (_not$to$have$been$cal = new chai.Assertion(actual).not.to.have.been.called).with.apply(_not$to$have$been$cal, expected);
   });
 };
